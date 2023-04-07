d17dc7013466b95cb14e3151362998d7c0b947c1
Prashant Srivastava <prashantsrivastava381@gmail.com>
2023-04-07 17:35:11 +0530

    Databse connected locally

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import app from './app.js';
+import connectDB from './database/db.js';
+
+connectDB();
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
