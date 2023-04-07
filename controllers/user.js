1548a92014c5b6454651659df48e5fd60d880560
Prashant Srivastava <prashantsrivastava381@gmail.com>
2023-04-07 18:03:27 +0530

    Model added of user (Schema)

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,13 @@
-export const getAllUsers = (req,res)=>{
-    res.send("All Users");
-}
+import User from '../models/user.js'
+
+export const getAllUsers = async(req,res)=>{
+    const user = await User.create({
+        name:'Prashant',
+        email:'prashantsrivastava381@gmail.com',     
+    })
+    if(!user){
+        return res.status(400).json({success:false})
+    }else{
+        return res.status(200).json({success:true,data:user})
+    }
+};
